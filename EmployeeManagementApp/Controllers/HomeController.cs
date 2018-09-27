fb6e202d33bc37b895262e9e25a4cab47d1e0576
Vanillasushi <Vanillasushi@users.noreply.github.com>
2018-09-26 19:47:41 -0500

    Week 5 - Assignment Unable to create date-picker.

---
+++ EmployeeManagementApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManagementApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Models.EmployeesDBEntities _db = new Models.EmployeesDBEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(_db.Employees.ToList());
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Exclude="Id")] Models.Employee employeeToAdd)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            _db.Employees.Add(employeeToAdd);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- EmployeeManagementApp/Properties/AssemblyInfo.cs
+++ EmployeeManagementApp/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("91b58fd1-3c3d-40e3-a942-2006651b306b")]
+[assembly: Guid("8995477b-b1e9-467e-bec8-3b95a0662235")]
 
 // Version information for an assembly consists of the following four values:
 //
